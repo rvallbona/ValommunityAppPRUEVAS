f74a55aa301af4db265be6fa0cfbd54d653d569e
Ramon Vallbona <98883990+rvallbona@users.noreply.github.com>
2022-11-14 19:21:45 +0100

    Fragments

--- app/src/main/java/com/example/valommunity/MainActivity.kt
+++ app/src/main/java/com/example/valommunity/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import com.example.valommunity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -21,5 +22,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.helloButton.setOnClickListener {
+            Toast.makeText(this, "Hola classe", Toast.LENGTH_LONG).show()
+        }
+
+        binding.helloText.setOnLongClickListener {
+            val snack = Snackbar.make(this, it, "Vols canviar el text?", Snackbar.LENGTH_LONG)
+            snack.setAction("Sí") {
+                binding.helloText.text = "Text canviat"
+            }
+            snack.show()
+
+            true
+        }
     }
 }
